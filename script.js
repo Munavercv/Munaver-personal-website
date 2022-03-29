eb29ca737303ac98e83d6205107f5d12da0d0d74
Munavercv <munavermunu80@gmail.com>
2022-03-29 14:27:40 +0530

    commented skills section                                                         hide full-body while loading

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ let loadrel = document.querySelector(".loaderelement");
 
 window.addEventListener("load", function(){
     loadrel.style.display = "none";
+    document.getElementById("full-body").style.display = "block";
 });
